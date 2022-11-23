8cfb5ccd7f59738380903e6d6c03ce97ad052cc8
sagegod <cdeng1999@gmail.com>
2022-11-22 20:35:12 -0800

    added packages

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+
+
+function App() {
+    return (
+      <div>
+        <Router>
+          <div>
+          
+            <div>
+                hello
+            </div>
+           
+          </div>
+        </Router>
+      </div>
+    );
+  }
+  
+  export default App;
